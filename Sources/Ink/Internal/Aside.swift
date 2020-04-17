7962da0639840575e90d9d1738a4a826a2510bb6
Todd Olsen <todd.olsen@me.com>
2020-04-17 16:15:39 -0400

    Add `<aside>` element using `^` markdown symbol.

---
+++ Sources/Ink/Internal/Aside.swift
@@ -0,0 +1,43 @@
+/**
+*  Ink
+*  Copyright (c) John Sundell 2019
+*  MIT license, see LICENSE file for details
+*/
+
+internal struct Aside: Fragment {
+    var modifierTarget: Modifier.Target { .asides }
+
+    private var text: FormattedText
+
+    static func read(using reader: inout Reader) throws -> Aside {
+        try reader.read("^")
+        try reader.readWhitespaces()
+
+        var text = FormattedText.readLine(using: &reader)
+
+        while !reader.didReachEnd {
+            switch reader.currentCharacter {
+            case \.isNewline:
+                return Aside(text: text)
+            case "^":
+                reader.advanceIndex()
+            default:
+                break
+            }
+
+            text.append(FormattedText.readLine(using: &reader))
+        }
+
+        return Aside(text: text)
+    }
+
+    func html(usingURLs urls: NamedURLCollection,
+              modifiers: ModifierCollection) -> String {
+        let body = text.html(usingURLs: urls, modifiers: modifiers)
+        return "<aside><p>\(body)</p></aside>"
+    }
+
+    func plainText() -> String {
+        text.plainText()
+    }
+}
